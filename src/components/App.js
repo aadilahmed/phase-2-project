f527f263fa9e48570326ac275df70a1ff4d0c297
Aadil Ahmed <aadilahmed0@gmail.com>
2023-06-26 19:49:26 -0700

    add working fetch to game db

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,10 @@
+import React from "react";
+import GamePage from "./GamePage";
+
 function App() {
   return (
     <div className="App">
+      <GamePage />
     </div>
   );
 }
--- src/components/GamePage.js
+++ src/components/GamePage.js
@@ -1,6 +1,17 @@
 import React, { useEffect, useState } from "react";
 
 function GamePage() {
+  const [games, setGames] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3333/games")
+      .then((response) => response.json())
+      .then((data) => setGames(data));
+  }, []);
+
+  const gamesToDisplay = games.map((game) => game.name);
+
+  return <ul>{gamesToDisplay}</ul>;
 }
 
 export default GamePage;
