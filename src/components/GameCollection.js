1e07585205934cf1c276719ce97dd7ea069a378b
Aadil Ahmed <aadilahmed0@gmail.com>
2023-06-27 02:30:30 -0700

    send game data as props from GameCollection to GameCard

--- src/components/GameCollection.js
+++ src/components/GameCollection.js
@@ -2,7 +2,16 @@ import React, { useEffect, useState } from "react";
 import { Card } from "semantic-ui-react";
 
 function GameCollection({ games }) {
-  
+  const gamesToDisplay = games.map((game) => {
+    <GameCard
+      key={game.id}
+      name={game.name}
+      background_image={game.background_image}
+      rating={game.rating}
+    />;
+  });
+
+  return <Card.Group itemsPerRow={6}>{gamesToDisplay}</Card.Group>;
 }
 
 export default GameCollection;
